25662e11dbb54bf898eef4024a5f5903773f44e5
Ivan Velickovic <i.velickovic@unsw.edu.au>
2025-09-01 20:20:11 +1000

    Add HiFive Premier P550 U-Boot
    Signed-off-by: Ivan Velickovic <i.velickovic@unsw.edu.au>

--- flake.nix
+++ flake.nix
@@ -362,6 +362,30 @@
           ''
         ;
 
+        hifiveP550Riscv64Uboot = pkgs.pkgsCross.riscv64.buildUBoot rec {
+            extraMeta.platforms = [ "riscv64-linux" ];
+            version = "u-boot-2024.01-EIC7X";
+            defconfig = "hifive_premier_p550_defconfig";
+
+            filesToInstall = [
+              ".config"
+              "u-boot.bin"
+            ];
+
+            # Either we add these flags to ignore these warnings that show up as errors
+            # or we have to use an older GCC.
+            prePatch = ''
+              echo "KBUILD_CFLAGS += -Wno-implicit-function-declaration -Wno-incompatible-pointer-types -Wno-int-conversion" >> Makefile
+            '';
+
+            src = pkgs.fetchFromGitHub {
+              owner = "eswincomputing";
+              repo = "u-boot";
+              rev = "27cda8f697e3990b4bea728c1066733259b7aa95";
+              hash = "sha256-0vqd3QdnBAuwA74PxDVLuVkcPGEYyMWsCFX3zpS0LLA=";
+            };
+        };
+
         star64Riscv64Uboot = pkgs.pkgsCross.riscv64.buildUBoot rec {
             extraMeta.platforms = [ "riscv64-linux" ];
             version = ubootVersion;
@@ -624,6 +648,8 @@
 
         packages.nanopir5c-aarch64-uboot = nanopir5cAarch64Uboot;
         packages.nanopir5c-aarch64-image = nanopir5cAarch64Image;
+
+        packages.hifivep550-riscv64-uboot = hifiveP550Riscv64Uboot;
 
         packages.tx2-uboot = tx2Uboot;
 
